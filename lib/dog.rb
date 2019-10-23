7a5119eb235d7e84bc5e533fc9ee91ab539267a7
Carla Holcomb <carla.programmer@gmail.com>
2019-10-23 17:26:57 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def initialize(name)
+    @name = name
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
